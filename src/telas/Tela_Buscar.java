d1cdafcafcc2ff5494345b91536896f8221f61f4
kisuk <80980217+kekisuk@users.noreply.github.com>
2022-01-03 21:23:42 -0300

    Update Tela_Buscar.java

--- src/telas/Tela_Buscar.java
+++ src/telas/Tela_Buscar.java
@@ -1,9 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package telas;
+
+/**
+ *
+ * AUTORES: Francisca Kelen Ferreira dos Santos, Mateus Eugênio de Andrade, Thiago Natalys Ribeiro da Silva, Olavo Cartaxo.
+ */
 
 import DAO.Conexao;
 import DAO.UsuarioDAO;
