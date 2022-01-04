1165f86b51a4230c5e0d4fe2be6bb7bad5a5e4e5
kisuk <80980217+kekisuk@users.noreply.github.com>
2022-01-03 21:25:54 -0300

    Update Tela_Menu.java

--- src/telas/Tela_Menu.java
+++ src/telas/Tela_Menu.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package telas;
 
 /**
  *
- * @author Olavo Cartaxo
+ * AUTORES: Francisca Kelen Ferreira dos Santos, Mateus Eugênio de Andrade, Thiago Natalys Ribeiro da Silva, Olavo Cartaxo.
  */
+
 public class Tela_Menu extends javax.swing.JFrame {
 
     /**
