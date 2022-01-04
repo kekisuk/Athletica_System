d0ce5b35cea6e3a9e392c80ab36235a46b41a94f
kisuk <80980217+kekisuk@users.noreply.github.com>
2022-01-03 21:15:03 -0300

    Update ExecuteSQL.java

--- src/DAO/ExecuteSQL.java
+++ src/DAO/ExecuteSQL.java
@@ -1,16 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
 import java.sql.*;
 
 /**
  *
- * @author Olavo Cartaxo
+ * AUTORES: Francisca Kelen Ferreira dos Santos, Mateus Eugênio de Andrade, Thiago Natalys Ribeiro da Silva, Lucas Antônio Ferreira Almeida, Olavo Cartaxo
  */
+
 public class ExecuteSQL {
     private Connection con;
     
